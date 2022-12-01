17cce37036d2fa847f429f076781130c36dfd6dc
Matt Macdonald <mksmacdonald@gmail.com>
2022-12-01 12:07:26 -0800

    read file mvp

--- main.py
+++ main.py
@@ -3,11 +3,19 @@ from pathlib import Path
 commit_tracker = Path.cwd() / "commit_tracker.txt"
 
 
+def retrieve_file(filename):
+    with open(filename, "r", encoding="utf-8") as file:
+        return file.read()
+
+
 def write_file(filename):
     with open(filename, "a", encoding="utf-8") as file:
         file.write("Haha, im so smart. ha ha ha.... i need a job\n")
 
 
 if __name__ == "__main__":
-    print("Hello world")
+    current_tracker = retrieve_file(commit_tracker)
+
+    print(current_tracker)
+
     write_file(commit_tracker)
