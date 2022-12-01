751f76140654be06e48c299dd89ef577b70be3a4
Matt Macdonald <mksmacdonald@gmail.com>
2022-12-01 12:05:20 -0800

    file writing MVP

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
+from pathlib import Path
+
+commit_tracker = Path.cwd() / "commit_tracker.txt"
+
+
+def write_file(filename):
+    with open(filename, "a", encoding="utf-8") as file:
+        file.write("Haha, im so smart. ha ha ha.... i need a job\n")
+
+
 if __name__ == "__main__":
     print("Hello world")
+    write_file(commit_tracker)
