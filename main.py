1c056130ac9988a864c8ac3f28914a06dc18990d
Matt Macdonald <mksmacdonald@gmail.com>
2022-12-01 13:14:46 -0800

    Heres another commit

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from typing import List
 from pathlib import Path
-import datetime
+from RPA.Windows import Windows
+
+win = Windows()
+
+
 
 from file_handler import (
     retrieve_file_as_list,
@@ -19,6 +23,33 @@ def update_commits_count_tracker():
 
     write_list_to_file(COMMIT_TRACKER_PATH, updated_list)
 
+def open_terminal():
+    win.windows_run("cmd")
+
+def get_terminal_focus():
+    return win.control_window('subname:"cmd.exe"')
+
+def navigate_to_repo_directory(element):
+    repo_path = Path.cwd().as_posix()
+    enter_command_in_terminal(element, f"cd {repo_path}")
+
+
+def enter_command_in_terminal(element, command):
+    win.send_keys(element, command, send_enter=True)
+
+
+def make_commit():
+    open_terminal()
+    terminal_element = get_terminal_focus()
+    navigate_to_repo_directory(terminal_element)
+    enter_command_in_terminal(terminal_element, f"git add commit_tracker.txt")
+    enter_command_in_terminal(terminal_element, f'git commit -m "Heres another commit"')
+    enter_command_in_terminal(terminal_element, f"git push")
+    win.close_window(terminal_element)
+
 
 if __name__ == "__main__":
     update_commits_count_tracker()
+    make_commit()
+
+    print("Completed")
